1283241f660a531e5c2ed11011f19146b4c1f062
Danielle <daniellezelin@gmail.com>
2022-12-02 11:32:39 -0500

    Display directions on show page and increment step count

--- app/models/direction.rb
+++ app/models/direction.rb
@@ -1,3 +1,11 @@
 class Direction < ApplicationRecord
   belongs_to :recipe
+
+  before_create :update_step_number
+
+  private
+
+  def update_step_number
+    self.step_number = recipe.directions.count + 1
+  end
 end
